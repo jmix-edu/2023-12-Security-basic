d911c078c54e39743e3f76316b6a1d506da5bc5e
vashenkoda <d.vaschenko@haulmont.net>
2023-11-21 14:45:50 +0300

    Initial commit

---
+++ src/main/java/com/company/jmixpm/screen/project/ProjectEdit.java
@@ -0,0 +1,15 @@
+package com.company.jmixpm.screen.project;
+
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.User;
+import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@UiController("Project.edit")
+@UiDescriptor("project-edit.xml")
+@EditedEntityContainer("projectDc")
+public class ProjectEdit extends StandardEditor<Project> {
+
+
+}
